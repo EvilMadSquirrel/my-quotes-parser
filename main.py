6ff3406b2f47b42b18ccb2c27ea8d6488b53b344
EvilMadSquirrel <minichev.s.l@gmail.com>
2021-10-20 13:49:22 +0300

    результат

--- main.py
+++ main.py
@@ -1,18 +1,41 @@
 import requests
 from bs4 import BeautifulSoup
+import json
 
 
 def get_html(page_number=1):
     num = page_number
     response = requests.get(f'https://quotes.toscrape.com/page/{num}/')
     response.raise_for_status()
-    return response.text
+    return (response.text, True)
+
+
+def parse_html(page_number=1):
+    raw_html = get_html(page_number)[0]
+    soup = BeautifulSoup(raw_html, 'html.parser')
+    quotes = soup.find_all('div', {'class': 'quote'})
+    page_result = {'quotes': []}
+    for quote in quotes:
+        text: str = quote.find('span', {'class': 'text'}).text
+        author = quote.find('small', {'class': 'author'}).text
+        raw_tags = quote.find_all('a', {'class': 'tag'})
+        tags = []
+        for tag in raw_tags:
+            tags.append(tag.text)
+        page_result['quotes'].append(
+            {'text': text, 'author': author, 'tags': tags})
+    return page_result
 
 
 def main():
-    raw_html = get_html(1)
-    soup = BeautifulSoup(raw_html, 'html.parser')
-    print(soup.prettify())
+
+    results = []
+    for page in range(1, 11):
+        result = parse_html(page)
+        results.append(result)
+
+    with open('results.json', 'w') as file:
+        json.dump(results, file)
 
 
 if __name__ == '__main__':
